57a20f2ba6b7f40ad6ed01f17a615eea5d2d1fe7
darayar.online <186970865+Darayabike0@users.noreply.github.com>
2026-02-12 04:52:20 +0700

    Add files via upload

---
+++ app/Battery System Admin/script.js
@@ -0,0 +1,18 @@
+API S3:
+https://8495bd97bd70a9bb9be1ea903f3ab66b.r2.cloudflarestorage.com/darayar
+
+Public Development URL
+Expose the contents of this R2 bucket to the internet through the Public Development URL when enabled.
+
+https://pub-9ba95f4e6bfb4adaa90b66c08887a631.r2.dev
+
+R2 Data Catalog
+Add an Apache Iceberg REST catalog, to allow query engines to manage and query Iceberg tables stored in R2.
+
+Catalog URI:
+
+https://catalog.cloudflarestorage.com/8495bd97bd70a9bb9be1ea903f3ab66b/darayar
+Click to copy
+Warehouse Name:
+
+8495bd97bd70a9bb9be1ea903f3ab66b_darayar
